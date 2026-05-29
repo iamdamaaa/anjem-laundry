7c6dd1beb6388340217e0c3b79b23d84c99e8911
iamdamaaa <14m.dama@gmail.com>
2026-05-29 11:32:52 +0700

    fix catch-all route optional

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 // Menangkap semua URL (seperti /services, /login) kecuali /api, 
 // lalu memberikan file React index.html
-Route::get('/{any}', function () {
+Route::get('/{any?}', function () {
     $path = base_path('../frontend/dist/index.html');
     if (file_exists($path)) {
         return file_get_contents($path);
