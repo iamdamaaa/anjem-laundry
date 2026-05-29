9e90635c88a8790cae7fbbbfaf094c439af05e37
iamdamaaa <14m.dama@gmail.com>
2026-05-29 10:32:35 +0700

    perbaikan kode

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->prepend(\App\Http\Middleware\ForceJsonResponse::class);
+        $middleware->api(prepend:[\App\Http\Middleware\ForceJsonResponse::class]);
         $middleware->alias([
             'role' => \App\Http\Middleware\AuthorizeRole::class,
         ]);
